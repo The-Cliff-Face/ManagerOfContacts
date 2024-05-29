128fea976c640a6c7d749244687a151bcfdf1415
Jacob Plotz <jacobplotz@knights.ucf.edu>
2024-05-29 14:47:17 -0400

    In-progress of adding input validation to backend

--- LAMPAPI/AddContact.php
+++ LAMPAPI/AddContact.php
@@ -1,4 +1,8 @@
 <?php
+	
+	$phoneRegex = '/^(\+1\s?)?(\(?\d{3}\)?[\s.-]?)?\d{3}[\s.-]?\d{4}$/';
+	$emailRegex = '/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/';
+
 	$inData = getRequestInfo();
 
 	$firstName = $inData["firstName"];
@@ -8,9 +12,26 @@
 	$userId = $inData["userId"];
 
 	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+
 	if ($conn->connect_error)
 	{
 		returnWithError( $conn->connect_error );
+	}
+	elseif ($firstName == "")
+	{
+		returnWithError("Invalid First Name");
+	}
+	elseif ($lastName == "")
+	{
+		returnWithError("Invalid Last Name");
+	}
+	elseif (!preg_match($phoneRegex, $phone))
+	{
+		returnWithError("Invalid Phone");
+	}
+	elseif (!preg_match($emailRegex, $email))
+	{
+		returnWithError("Invalid Email");
 	}
 	else
 	{
